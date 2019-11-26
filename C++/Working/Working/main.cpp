bef39936a3fa3d78f5b5790df2f572a74a245f07
RHS_Air <rhs_air@MacBook-Air.local>
2019-11-26 11:44:39 -0500

    Added Working Folder

---
+++ C++/Working/Working/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Working
+//
+//  Created by RHS_Air on 11/26/19.
+//  Copyright © 2019 RHS_Air. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

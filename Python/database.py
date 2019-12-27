737bd68d965e369ea9f51d9b22a68069c465605f
RHS_Air <rhs_air@MacBook-Air.local>
2019-12-26 20:13:52 -0500

    working

--- Python/database.py
+++ Python/database.py
@@ -5,14 +5,12 @@ conn = sqlite3.connect('products.db')
 c = conn.cursor()
 
 #conn.execute('''CREATE TABLE prod (name text, review real, qty real, price real)''')
-#conn.execute(" INSERT INTO prod VALUES ('IPhone 11' , '5', 87, 999)")
 
+#c.execute("INSERT INTO prod VALUES ('InstantPot', 4.5, 100, 120)")
 
-#c.execute("DELETE FROM prod WHERE name ='IPhone 11'")
-c.execute("SELECT * FROM prod WHERE name = 'IPhone 11' ")
+c.execute("SELECT review FROM prod WHERE price=999")
+
 print(c.fetchall())
-
-
 
 conn.commit()
 conn.close()
